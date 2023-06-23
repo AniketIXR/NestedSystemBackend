4c34f65a748b09aaf594dd46e21aa1b6dfd1c536
AniketIXR <aniketroy2003@gmail.com>
2023-06-23 13:14:21 +0530

    cors fixed

--- app.js
+++ app.js
@@ -16,10 +16,15 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000, 
 }));
 
+// app.use(cors({
+//     origin: process.env.CLIENT_URL || 'http://localhost:3001',
+//     credentials: true
+//   }));
+
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3001',
-    credentials: true
-  }));
+  origin: '*',
+  credentials: true
+}));
 
   const CURRENT_USER_ID = "647b11ce3dd2cc06163d72ed";
   
